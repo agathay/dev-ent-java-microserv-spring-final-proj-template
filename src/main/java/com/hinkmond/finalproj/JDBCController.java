f5d1383479dda4f797def57b6fa4e0edd8336e87
agathay <agathay@yahoo.com>
2021-03-28 12:44:28 -0700

    Retrieve latest Cryptocurrency prices

--- src/main/java/com/hinkmond/finalproj/JDBCController.java
+++ src/main/java/com/hinkmond/finalproj/JDBCController.java
@@ -3,6 +3,14 @@ package com.hinkmond.finalproj;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.web.bind.annotation.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import com.fasterxml.jackson.databind.*;
+
+import java.util.*;
+import java.io.*;
 
 
 @RestController
@@ -10,64 +18,63 @@ public class JDBCController {
     private final static String KEYFILEPATH = "./keyFile.key";
 
     @CrossOrigin
-    @RequestMapping(value = "/helloworld", method = RequestMethod.GET)
-    public String printCryptTest() {
-        AESUtils aesUtils = new AESUtils();
-
-        String encryptedStr = aesUtils.encrypt("Hello World!", KEYFILEPATH);
-        return ("Decrypt = " + aesUtils.decrypt(encryptedStr, KEYFILEPATH));
-    }
-
-    @CrossOrigin
-    @RequestMapping(value = "/printAllUsers", method = RequestMethod.GET)
+    @RequestMapping(value = "/printAllCrypto", method = RequestMethod.GET)
     public String printAllUsers() {
         JdbcTemplate jdbcTemplate = JDBCConnector.getJdbcTemplate();
         StringBuilder resultStr = new StringBuilder();
 
-        String queryStr = "SELECT * from user_info;";
+        String queryStr = "SELECT * from crypto_info;";
         SqlRowSet sqlRowSet = jdbcTemplate.queryForRowSet(queryStr);
         while (sqlRowSet.next()) {
-            resultStr.append(sqlRowSet.getString("user_id")).append(", ")
-                    .append(sqlRowSet.getString("first_name")).append(", ")
-                    .append(sqlRowSet.getString("last_name")).append(", ")
-                    .append(sqlRowSet.getString("addr")).append(", ")
-                    .append(sqlRowSet.getString("phone")).append(", ")
-                    .append(sqlRowSet.getString("email")).append(", ")
-                    .append(sqlRowSet.getString("created_at"))
+            resultStr.append(sqlRowSet.getString("symbol")).append(" -- ")
+                    .append(sqlRowSet.getString("name")).append(", ")
+                    .append('$' + sqlRowSet.getString("price")).append(" Updated: ")
+                    .append(sqlRowSet.getString("updated_at"))
                     .append("\n");
         }
-        return ("SELECT * from user_info:\n" + resultStr);
+        return ("" + resultStr);
     }
 
     @CrossOrigin
-    @RequestMapping(value = "/addUser", method = RequestMethod.POST)
-    public String addUser(@RequestBody AddUserData addUserData) {
+    @RequestMapping(value = "/updateCryptoPrice", method = RequestMethod.POST)
+    public String updateLatest(@RequestBody UpdateCryptoData updateCryptoData) throws IOException {
+        // https://nomics.com/docs/
+
+        String crypto = updateCryptoData.getSymbol();
+        // Only BCH, BTC, ETH, LTC are supported in this app.
+        if (!(crypto.equals("BCH") || crypto.equals("BTC") || crypto.equals("ETH") || crypto.equals("LTC")))
+            return "Only BCH -- Bitcoin Cash, BTC -- Bitcoin, ETH -- Ethereum and LTC -- Litecoin are supported in this app.";
+
+        URL url = new URL("https://api.nomics.com/v1/currencies/sparkline?key=8427cbef660536f213a6b2d5706a4a27&ids="
+                + crypto + "&start=2021-03-25T00%3A00%3A00Z");
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestProperty("accept", "application/json");
+
+        InputStream responseStream = connection.getInputStream();
+
+        // Read from crypto prices datafeed
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int nRead;
+        byte[] data = new byte[1024];
+        while ((nRead = responseStream.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+        buffer.flush();
+        byte[] byteArray = buffer.toByteArray();
+
+        // Transform to Json
+        String jsonString = new String(byteArray, java.nio.charset.StandardCharsets.UTF_8);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(jsonString.substring(1, jsonString.length() - 1));
+
+        // Update price in DB
+        List<String> list = mapper.convertValue(rootNode.get("prices"), ArrayList.class);
+        String price = list.get(list.size() - 1);
         JdbcTemplate jdbcTemplate = JDBCConnector.getJdbcTemplate();
-        String queryStr = "INSERT INTO user_info (first_name, last_name, addr, email) " +
-                "VALUES (" +
-                "'" + addUserData.getFirstName() + "'," +
-                "'" + addUserData.getLastName() + "'," +
-                "'" + addUserData.getAddress() + "'," +
-                "'" + addUserData.getEmail() + "'" +
-                ");";
+        String queryStr = "UPDATE crypto_info SET price = " + price + " WHERE symbol = '" + crypto + "'";
         int rowsUpdated = jdbcTemplate.update(queryStr);
-        return ("Rows updated: " + rowsUpdated);
-    }
 
-    @CrossOrigin
-    @RequestMapping(value = "/printAllAccts", method = RequestMethod.GET)
-    public String printAllAccts() {
-        JdbcTemplate jdbcTemplate = JDBCConnector.getJdbcTemplate();
-        StringBuilder resultStr = new StringBuilder();
-
-        String queryStr = "SELECT * from acct_info;";
-        SqlRowSet sqlRowSet = jdbcTemplate.queryForRowSet(queryStr);
-        while (sqlRowSet.next()) {
-            resultStr.append(sqlRowSet.getString("user_id")).append(", ")
-                    .append(sqlRowSet.getString("acct_num")).append(", ")
-                    .append(sqlRowSet.getString("balance"))
-                    .append("\n");
-        }
-        return ("SELECT * from acct_info:\n" + resultStr);
+        return crypto + " has been updated to price " + price;
     }
 }
---
+++ src/main/java/com/hinkmond/finalproj/UpdateCryptoData.java
@@ -0,0 +1,12 @@
+package com.hinkmond.finalproj;
+
+public class UpdateCryptoData {
+    private String symbol;
+
+    public String getSymbol() {
+        return symbol;
+    }
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+}
